5bc775ea07a154aacb2dae443de834256266c3da
kylethefox <halobuster1982>
2018-07-19 22:47:02 -0400

    Calling blockcypher

--- index.py
+++ index.py
@@ -4,10 +4,6 @@ import sys
 import requests
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
     
     token = "ae1cd45d2e5a4ee7a8cdfadebd7b8f7"
     url = "https://api.blockcypher.com/v1/eth/main"
