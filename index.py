219ac6d9e9cecd90935885a423986f8bf548a24b
kylethefox <halobuster1982>
2018-07-19 22:12:21 -0400

    Calling blockcypher

--- index.py
+++ index.py
@@ -1,12 +1,19 @@
 import json
 import datetime
-
+import sys
+import requests
 
 def handler(event, context):
     data = {
         'output': 'Hello World',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
+    
+    token = "ae1cd45d2e5a4ee7a8cdfadebd7b8f7"
+    url = "https://api.blockcypher.com/v1/eth/main/addrs?token=" + token
+    r = requests.post(url)
+    print(r.text)
+  
     return {'statusCode': 200,
-            'body': json.dumps(data),
+            'body': json.dumps(r),
             'headers': {'Content-Type': 'application/json'}}
