6ddbfe53a5b76294e97099d231db45a98c74e1fd
kylethefox <halobuster1982>
2018-07-20 17:25:06 -0400

    Calling blockcypher

--- index.py
+++ index.py
@@ -9,8 +9,9 @@ def handler(event, context):
     token = "ae1cd45d2e5a4ee7a8cdfadebd7b8f7"
     url = "https://api.blockcypher.com/v1/eth/main"
     r = requests.get(url)
-    print(r.text)
+    print(context)
+    print(event)
   
     return {'statusCode': 200,
-            'body': r.json,
+            'body': json.loads(r.text),
             'headers': {'Content-Type': 'application/json'}}
