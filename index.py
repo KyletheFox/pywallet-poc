480bc4c46fe42a4cadcbe23a98bf0c01a187e096
kylethefox <halobuster1982>
2018-07-19 22:24:16 -0400

    Calling blockcypher

--- index.py
+++ index.py
@@ -10,8 +10,8 @@ def handler(event, context):
     }
     
     token = "ae1cd45d2e5a4ee7a8cdfadebd7b8f7"
-    url = "https://api.blockcypher.com/v1/eth/main/addrs?token=" + token
-    r = requests.post(url)
+    url = "https://api.blockcypher.com/v1/eth/main"
+    r = requests.get(url)
     print(r.text)
   
     return {'statusCode': 200,
